4a5dd612d13a0513fd0cb49b7f0890b0428e2ed9
mahdiakbari1 <mahdi75akbari@gmail.com>
2021-05-24 11:18:44 +0430

    Update Converter.php

--- src/Converter.php
+++ src/Converter.php
@@ -4,7 +4,10 @@ namespace Cafesource\Helper;
 
 class Converter
 {
-    public static $numbers = [
+    /**
+     * @var array numbers
+     */
+    const numbers = [
         'english' => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
         'persian' => ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'],
         'arabic'  => ['۰', '۱', '۲', '۳', '٤', '٥', '٦', '۷', '۸', '۹'],
@@ -17,11 +20,10 @@ class Converter
      *
      * @return string
      */
-    public static function toEnglish( $string )
+    public static function toEnglish( string $string ) : string
     {
-        $string = str_replace(self::$numbers[ 'persian' ], self::$numbers[ 'english' ], $string);
-        $string = str_replace(self::$numbers[ 'arabic' ], self::$numbers[ 'english' ], $string);
-        return $string;
+        $string = str_replace(static::numbers[ 'persian' ], static::numbers[ 'english' ], $string);
+        return str_replace(static::numbers[ 'arabic' ], static::numbers[ 'english' ], $string);
     }
 
     /**
@@ -31,11 +33,10 @@ class Converter
      *
      * @return string
      */
-    public static function toPersian( $string )
+    public static function toPersian( string $string ) : string
     {
-        $string = str_replace(self::$numbers[ 'english' ], self::$numbers[ 'persian' ], $string);
-        $string = str_replace(self::$numbers[ 'arabic' ], self::$numbers[ 'persian' ], $string);
-        return $string;
+        $string = str_replace(static::numbers[ 'english' ], static::numbers[ 'persian' ], $string);
+        return str_replace(static::numbers[ 'arabic' ], static::numbers[ 'persian' ], $string);
     }
 
     /**
@@ -45,10 +46,9 @@ class Converter
      *
      * @return string
      */
-    public static function toArabic( $string )
+    public static function toArabic( string $string ) : string
     {
-        $string = str_replace(self::$numbers[ 'english' ], self::$numbers[ 'arabic' ], $string);
-        $string = str_replace(self::$numbers[ 'persian' ], self::$numbers[ 'arabic' ], $string);
-        return $string;
+        $string = str_replace(static::numbers[ 'english' ], static::numbers[ 'arabic' ], $string);
+        return str_replace(static::numbers[ 'persian' ], self::numbers[ 'arabic' ], $string);
     }
 }
